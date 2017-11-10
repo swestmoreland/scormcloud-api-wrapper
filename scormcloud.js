07738ba889fa689fb1c0c53a3c81f33678c6e0fb
Steven Westmoreland <steven.westmoreland@gmail.com>
2017-11-10 12:58:07 -0600

    Keeping things DRY

--- scormcloud.js
+++ scormcloud.js
@@ -28,34 +28,27 @@ SCORMCloud.prototype.getCourseList = function (callback, filter, tags) {
     // A comma separated list of tags to filter results by. Results will include only courses which are tagged with every tag in the list.
     if (tags) url.searchParams.set('tags', tags);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
+        let data = [];
+        let courseList = _.isArray(json.rsp.courselist.course) ? json.rsp.courselist.course : [ json.rsp.courselist.course ];
 
-            console.log(util.inspect(json, false, null));
-
-            let data = [];
-            let courseList = _.isArray(json.rsp.courselist.course) ? json.rsp.courselist.course : [ json.rsp.courselist.course ];
-
-            courseList.forEach(function (course) {
-                data.push({
-                   "id":               course.id,
-                   "title":            course.title,
-                   "versions":         _.toInteger(course.versions),
-                   "registrations":    _.toInteger(course.registrations),
-                   "size":             _.toInteger(course.size),
-                   "tags":             _.flatten(_.toArray(course.tags)),
-                   "learningStandard": course.learningStandard,
-                   "createDate":       course.createDate
-                });
+        courseList.forEach(function (course) {
+            data.push({
+               "id":               course.id,
+               "title":            course.title,
+               "versions":         _.toInteger(course.versions),
+               "registrations":    _.toInteger(course.registrations),
+               "size":             _.toInteger(course.size),
+               "tags":             _.flatten(_.toArray(course.tags)),
+               "learningStandard": course.learningStandard,
+               "createDate":       course.createDate
             });
-
-            return callback(null, data);
         });
+
+        return callback(error, data);
 
     });
 }
@@ -80,38 +73,31 @@ SCORMCloud.prototype.getRegistrationList = function (callback, courseid, learner
     // Return registrations updated up to and including this timestamp.
     if (until) url.searchParams.set('until', until);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
+        let data = [];
+        let registrationList = _.isArray(json.rsp.registrationlist.registration) ? json.rsp.registrationlist.registration : [ json.rsp.registrationlist.registration ];
 
-            console.log(util.inspect(json, false, null))
-
-            let data = [];
-            let registrationList = _.isArray(json.rsp.registrationlist.registration) ? json.rsp.registrationlist.registration : [ json.rsp.registrationlist.registration ];
-
-            registrationList.forEach(function (registration) {
-                data.push({
-                   "id":                        registration.id,
-                   "courseId":                  registration.courseId,
-                   "courseTitle":               registration.courseTitle,
-                   "lastCourseVersionLaunched": registration.lastCourseVersionLaunched,
-                   "learnerId":                 registration.learnerId,
-                   "learnerFirstName":          registration.learnerFirstName,
-                   "learnerLastName":           registration.learnerLastName,
-                   "email":                     registration.email,
-                   "createDate":                registration.createDate,
-                   "firstAccessDate":           registration.firstAccessDate,
-                   "lastAccessDate":            registration.lastAccessDate,
-                   "completedDate":             registration.completedDate
-                });
+        registrationList.forEach(function (registration) {
+            data.push({
+               "id":                        registration.id,
+               "courseId":                  registration.courseId,
+               "courseTitle":               registration.courseTitle,
+               "lastCourseVersionLaunched": registration.lastCourseVersionLaunched,
+               "learnerId":                 registration.learnerId,
+               "learnerFirstName":          registration.learnerFirstName,
+               "learnerLastName":           registration.learnerLastName,
+               "email":                     registration.email,
+               "createDate":                registration.createDate,
+               "firstAccessDate":           registration.firstAccessDate,
+               "lastAccessDate":            registration.lastAccessDate,
+               "completedDate":             registration.completedDate
             });
-
-            return callback(null, data);
         });
+
+        return callback(error, data);
 
     });
 }
@@ -130,36 +116,29 @@ SCORMCloud.prototype.getInvitationList = function (callback, filter, coursefilte
     // A regular express that will be used to filter the list of invitations. Specifically only those invitations that are associated with courses whose courseid’s match the given expression will be returned in the list.
     if (coursefilter) url.searchParams.set('coursefilter', coursefilter);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
+        let data = [];
+        let invitationList = _.isArray(json.rsp.invitationlist.invitationInfo) ? json.rsp.invitationlist.invitationInfo : [ json.rsp.invitationlist.invitationInfo ];
 
-            console.log(util.inspect(json, false, null));
-
-            let data = [];
-            let invitationList = _.isArray(json.rsp.invitationlist.invitationInfo) ? json.rsp.invitationlist.invitationInfo : [ json.rsp.invitationlist.invitationInfo ];
-
-            invitationList.forEach(function (invitation) {
-                data.push({
-                   "id":                    invitation.id,
-                   "body":                  invitation.body,
-                   "courseId":              invitation.courseId,
-                   "subject":               invitation.subject,
-                   "url":                   invitation.url,
-                   "allowLaunch":           _.lowerCase(invitation.allowLaunch) === 'true' ? true : false,
-                   "allowNewRegistrations": _.lowerCase(invitation.allowNewRegistrations) === 'true' ? true : false,
-                   "public":                _.lowerCase(invitation.public) === 'true' ? true : false,
-                   "created":               _.lowerCase(invitation.created) === 'true' ? true : false,
-                   "createdDate":           invitation.createdDate
-                });
+        invitationList.forEach(function (invitation) {
+            data.push({
+               "id":                    invitation.id,
+               "body":                  invitation.body,
+               "courseId":              invitation.courseId,
+               "subject":               invitation.subject,
+               "url":                   invitation.url,
+               "allowLaunch":           _.lowerCase(invitation.allowLaunch) === 'true' ? true : false,
+               "allowNewRegistrations": _.lowerCase(invitation.allowNewRegistrations) === 'true' ? true : false,
+               "public":                _.lowerCase(invitation.public) === 'true' ? true : false,
+               "created":               _.lowerCase(invitation.created) === 'true' ? true : false,
+               "createdDate":           invitation.createdDate
             });
-
-            return callback(null, data);
         });
+
+        return callback(error, data);
 
     });
 }
@@ -175,20 +154,13 @@ SCORMCloud.prototype.getCourseTags = function (callback, courseid) {
     // The id of the course for which the tags will be retrieved.
     if (courseid) url.searchParams.set('courseid', courseid);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
+        let data = _.flatten(_.toArray(json.rsp.tags));
 
-            console.log(util.inspect(json, false, null));
-
-            let data = _.flatten(_.toArray(json.rsp.tags));
-
-            return callback(null, data);
-        });
+        return callback(error, data);
 
     });
 }
@@ -203,20 +175,34 @@ SCORMCloud.prototype.setCourseTags = function (callback, courseid, tags) {
     // A comma separated list of tags to set for the course.
     if (tags) url.searchParams.set('tags', tags);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
+        let data = _.has(json.rsp, 'success');
 
-            console.log(util.inspect(json, false, null));
+        return callback(error, data);
 
-            let data = _.has(json.rsp, 'success');
+    });
+}
 
-            return callback(null, data);
-        });
+SCORMCloud.prototype.addCourseTag = function (callback, courseid, tag) {
+
+    var url = new URL('api?method=rustici.tagging.addCourseTag', this.serviceUrl);
+
+    // The id of the course which the given tag will be associated with.
+    if (courseid) url.searchParams.set('courseid', courseid);
+
+    // The tag to associate with the course.
+    if (tag) url.searchParams.set('tag', tag);
+
+    this._get(url, function (error, json) {
+
+        if (error) return callback(error, json);
+
+        let data = _.has(json.rsp, 'success');
+
+        return callback(error, data);
 
     });
 }
@@ -228,20 +214,13 @@ SCORMCloud.prototype.getLearnerTags = function (callback, learnerid) {
     // The id of the learner for which the tags will be retrieved.
     if (learnerid) url.searchParams.set('learnerid', learnerid);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
+        let data = _.flatten(_.toArray(json.rsp.tags));
 
-            console.log(util.inspect(json, false, null));
-
-            let data = _.flatten(_.toArray(json.rsp.tags));
-
-            return callback(null, data);
-        });
+        return callback(error, data);
 
     });
 }
@@ -253,20 +232,13 @@ SCORMCloud.prototype.getRegistrationTags = function (callback, regid) {
     // The id of the registration for which the tags will be retrieved.
     if (regid) url.searchParams.set('regid', regid);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
+        let data = _.flatten(_.toArray(json.rsp.tags));
 
-            console.log(util.inspect(json, false, null));
-
-            let data = _.flatten(_.toArray(json.rsp.tags));
-
-            return callback(null, data);
-        });
+        return callback(error, data);
 
     });
 }
@@ -279,35 +251,28 @@ SCORMCloud.prototype.getAccountInfo = function (callback) {
 
     var url = new URL('api?method=rustici.reporting.getAccountInfo', this.serviceUrl);
 
-    this._get(url, function (error, rawData) {
+    this._get(url, function (error, json) {
 
-        console.log(rawData);
-        parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
-            if (err) throw err;
+        if (error) return callback(error, json);
 
-            if (json.rsp.stat === 'fail') return callback(new Error(json.rsp.err.msg), json.rsp.err);
-
-            console.log(util.inspect(json, false, null));
-
-            let data = {
-                "email":                  json.rsp.account.email,
-                "firstname":              json.rsp.account.firstname,
-                "lastname":               json.rsp.account.lastname,
-                "company":                json.rsp.account.company,
-                "accounttype":            json.rsp.account.accounttype,
-                "reglimit":               _.toInteger(json.rsp.account.reglimit),
-                "strictlimit":            _.lowerCase(json.rsp.account.strictlimit) === 'true' ? true : false,
-                "createdate":             json.rsp.account.createdate,
-                "usage": {
-                    "monthstart":         json.rsp.account.usage.monthstart,
-                    "regcount":           _.toInteger(json.rsp.account.usage.regcount),
-                    "totalregistrations": _.toInteger(json.rsp.account.usage.totalregistrations),
-                    "totalcourses":       _.toInteger(json.rsp.account.usage.totalcourses)
-                }
+        let data = {
+            "email":                  json.rsp.account.email,
+            "firstname":              json.rsp.account.firstname,
+            "lastname":               json.rsp.account.lastname,
+            "company":                json.rsp.account.company,
+            "accounttype":            json.rsp.account.accounttype,
+            "reglimit":               _.toInteger(json.rsp.account.reglimit),
+            "strictlimit":            _.lowerCase(json.rsp.account.strictlimit) === 'true' ? true : false,
+            "createdate":             json.rsp.account.createdate,
+            "usage": {
+                "monthstart":         json.rsp.account.usage.monthstart,
+                "regcount":           _.toInteger(json.rsp.account.usage.regcount),
+                "totalregistrations": _.toInteger(json.rsp.account.usage.totalregistrations),
+                "totalcourses":       _.toInteger(json.rsp.account.usage.totalcourses)
             }
+        }
 
-            return callback(null, data);
-        });
+        return callback(error, data);
 
     });
 }
@@ -329,19 +294,18 @@ SCORMCloud.prototype._get = function (url, callback) {
 
     https.get(url, (res) => {
 
+        console.log(res);
+
         const contentType = res.headers['content-type'];
         const { statusCode } = res;
 
         let error;
         if (statusCode !== 200) {
-            error = new Error(`Request Failed. Status Code: ${statusCode}`);
-        } else if (!/^text\/xml/.test(contentType)) {
-            error = new Error(`Invalid content-type. Expected text/xml but received ${contentType}`);
+            error = new Error(`Request failed (${statusCode})`);
         }
         if (error) {
-            console.error(error.message);
             res.resume();
-            return;
+            return callback(error);
         }
 
         res.setEncoding('utf8');
@@ -349,11 +313,23 @@ SCORMCloud.prototype._get = function (url, callback) {
         let rawData = '';
         res.on('data', (chunk) => { rawData += chunk; });
         res.on('end', () => {
-            callback(null, rawData);
+            console.log(rawData);
+            parseString(rawData, { explicitArray: false, mergeAttrs: true }, function (err, json) {
+                if (err) throw err;
+
+                console.log(util.inspect(json, false, null));
+
+                let error;
+                if (json.rsp.stat === 'fail') {
+                    error = new Error(json.rsp.err.msg);
+                }
+
+                callback(error, json);
+            });
         });
 
     }).on('error', (e) => {
-        console.error(`Got error: ${e.message}`);
+        console.error(`Error: ${e.message}`);
     });
 
 }
